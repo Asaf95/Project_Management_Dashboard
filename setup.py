586890710d063bc4d4d538c7f60e723b40dd4780
AsafB <asafbm95@gmail.com>
2022-11-12 21:15:10 +0200

    Add init files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Project_Management_Dashboard',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='asaf',
+    author_email='',
+    description=''
+)
